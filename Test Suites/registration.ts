400b0d620efc3bef81518a5aea9e2b7d6ff59921
achraf bouchouik <abdoliloaf@gmail.com>
2022-10-20 18:09:24 +0100

    add test suite

--- Test Suites/registration.ts
+++ Test Suites/registration.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>021853da-b03f-41d7-87be-bbe33edf7d8f</testSuiteGuid>
+   <testCaseLink>
+      <guid>d4bd438b-1a41-4aa0-b96b-4c0e53fbd7ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/registration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
